eb61c7175750844a797863acd0fda329055bd903
Danilo Menezes <danilo.menezes@devoteam.com>
2019-11-25 13:10:25 +0000

    fix sql query

--- modules/news/view.php
+++ modules/news/view.php
@@ -3,7 +3,7 @@ if (!defined('FLUX_ROOT')) exit;
 
 $news = Flux::config('FluxTables.CMSNewsTable'); 
 
-$sql = "SELECT title, body, link, author, created, modified FROM {$server->loginDatabase}.$news ORDER BY id DESC LIMIT ?";
+$sql = "SELECT title, body, link, author, created, modified FROM {$server->loginDatabase}.$news ORDER BY id DESC LIMIT 20";
 
 $sth = $server->connection->getStatement($sql);
 $sth->execute(array((int)Flux::config('CMSNewsLimit')));
